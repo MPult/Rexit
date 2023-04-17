e92c7ed51c38b2dec1262c73d43cd6668d625ea9
Maximilian Pult <it@optik-riede.de>
2023-04-17 13:23:22 +0200

    Add options for export formats

--- src/export.rs
+++ src/export.rs
@@ -3,19 +3,30 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::{fs, io};
 
+use super::cli;
+
+use crate::cli::Cli;
 use crate::AllChats;
 
 // Function to check what export format is desired and calls the apporopriate function below
-pub fn decide_export(all_chats: AllChats) {
-    // For now just defualt to exporting txt
-    export_to_txt(all_chats.clone());
-    export_to_json(all_chats.clone());
-    export_to_csv(all_chats);
+pub fn decide_export(all_chats: AllChats, cli: Cli) {
+
+    // Split the comma seperated format cli args into a array
+    let formats: Vec<&str> = cli.formats.split(",").collect();
+
+    // Run the appropriate function for each export format
+    for format in formats {
+        match format {
+            "json" => export_to_json(all_chats.clone()),
+            "csv" => export_to_csv(all_chats.clone()),
+            "txt" => export_to_txt(all_chats.clone()),
+            _ => println!("Not valid Format"),
+        }
+    }
 }
 
 // Function to export the chats into .txt files
 pub fn export_to_txt(all_chats: AllChats) {
-
     // Iterate over the individual chats / rooms (idk what to call it reddit uses the terms interchangibly)
     for chat in all_chats.chats {
         // Create the file for each chat / room
@@ -52,7 +63,6 @@ pub fn export_to_json(all_chats: AllChats) {
 
 // Function to export the chats into .csv files
 pub fn export_to_csv(all_chats: AllChats) {
-
     // Iterate over the individual chats / rooms (idk what to call it reddit uses the terms interchangibly)
     for chat in all_chats.chats {
         // Create the file for each chat / room
