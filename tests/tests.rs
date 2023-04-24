116d72e9d387fdcdc53bdab07eb2b6f1be197326
Maximilian Pult <it@optik-riede.de>
2023-04-24 18:39:04 +0200

    no

--- tests/tests.rs
+++ tests/tests.rs
@@ -9,6 +9,6 @@ fn login() {
 fn get_login() -> (String, String) {
     let username = std::env::var("REXIT_USERNAME").expect("Could not find username in env");
     let password = std::env::var("REXIT_PASSWORD").expect("Could not find password in env");
-
+    println!("{username} {password}");
     (username, password)
 }
